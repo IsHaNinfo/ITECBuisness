0c502458a83e38a61e5d567359e6f197a65a0ac2
ishaninfo56@gmail.com <120440668+IsHaNinfo@users.noreply.github.com>
2023-07-13 10:56:16 +0530

    update the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 function App() {
-  return <></>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/Home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
